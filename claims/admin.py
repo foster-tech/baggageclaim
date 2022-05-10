7a3edcf1fceb2c37645a2ab8aca134e2b860ee15
Jorge Garcia <garcia.jhp@gmail.com>
2022-05-09 21:44:29 -0300

    feat(admin): add models to django admin

---
+++ claims/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from claims.models import Claim1, Claim2, Claim3, Claim4
+
+admin.site.register(Claim1)
+admin.site.register(Claim2)
+admin.site.register(Claim3)
+admin.site.register(Claim4)
