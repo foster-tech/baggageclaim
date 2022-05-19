5d157de062c980f805f359325d84719b83d83075
Jorge Garcia <garcia.jhp@gmail.com>
2022-05-18 21:29:35 -0300

    fix(destination): remove humanize_profile in order to use the right function get_[field]_display

--- app/views.py
+++ app/views.py
@@ -16,13 +16,12 @@ def DestinationView(request):
     matches = []
     db_matches = Matches.objects.filter(Q(user1=request.user) | Q(user2=request.user))
     for m in db_matches:
-        print(model_to_dict(m)) # TODO REMOVE DEBUG
         other_profile = UserProfile.objects.get(user=m.user1) if m.user1 != request.user else UserProfile.objects.get(user=m.user2)
         fc = min(user_profile.filled_claims, other_profile.filled_claims)
         matches.append(
-            {'user': humanize_profile(other_profile), 'match': get_match_level(m.value, fc)}
-                if m.user1!=request.user
-                else {'user': humanize_profile(other_profile), 'match': get_match_level(m.value_reverse, fc)})
+            {'user': other_profile, 'match': get_match_level(m.value, fc)}
+                if m.user1 != request.user
+                else {'user': other_profile, 'match': get_match_level(m.value_reverse, fc)})
 
     context = {'matches': matches }
 
@@ -53,17 +52,3 @@ def get_match_level(score, filled_claims):
         elif score > 29.5: return 3 # Medium match
         elif score > 14.5: return 2 # High match
         else: return 1# Cloud 9
-
-def humanize_profile(user_profile):
-    '''Convert some fields on the Profile with theyr values'''
-    up = model_to_dict(user_profile)
-    up['gender'] = Genders.labels[up['gender']] if up['gender'] else ''
-    up['sexual_orientation'] = SexualOrientations.labels[up['sexual_orientation']] if up['sexual_orientation'] else ''
-    up['education'] = EducationLevels.labels[up['education']] if up['education'] else ''
-    up['religion'] = [Religions.labels[int(i)] for i in up['religion']] if up['religion'] else []
-    up['children'] = [Children.labels[int(i)] for i in up['children']] if up['children'] else []
-    up['destination'] = [Destinations.labels[int(i)] for i in up['destination']] if up['destination'] else []
-    up['selected_stamps'] = [Stamps.labels[int(i)] for i in up['selected_stamps']] if up['destination'] else []
-    return up
-
-
